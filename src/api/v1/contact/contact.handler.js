06f4dd78bdd600653bf540b2921c2c3af09c4a4f
Ian Grail <grailians@gmail.com>
2016-06-11 13:53:09 -0400

    feat: send Segment tracking for ReQLPro downloads (in case user's browser plugins is blocking tracking)

--- src/api/v1/contact/contact.handler.js
+++ src/api/v1/contact/contact.handler.js
@@ -1,28 +1,44 @@
-var Slack = require('../../../services/slack/slack-service.js');
-var Email = require('../../../services/email/email-service.js');
+var Slack = require('../../../services/slack/slack.service.js');
+var Email = require('../../../services/email/email.service.js');
+var Segment = require('../../../services/segment/segment.service.js');
 var actions = require('../../../data/actions.json');
 
 var handler = function(req, res, next) {
-    var payload = req.body;
-    console.log('New Capture:');
-    console.log(payload);
+  var payload = req.body;
+  console.log('New Capture:');
+  console.log(payload);
 
-    var action = actions[req.params.id];
+  var action = actions[req.params.id];
 
-    switch (action.type) {
-        case 'contact':
-            Email.newContact(action, payload, function(err, msg) {});
-            Slack.newContact(action, payload, function(err, msg) {});
-            break;
-        case 'download':
-            Email.newContact(action, payload, function(err, msg) {});
-            Slack.downloadCapture(action, payload, function(err, msg) {});
-            break;
-    }
+  switch (action.type) {
+    case 'contact':
+      Email.newContact(action, payload, function(err, msg) {});
+      Slack.newContact(action, payload, function(err, msg) {});
+      break;
+    case 'download':
+      Email.newContact(action, payload, function(err, msg) {});
+      Slack.downloadCapture(action, payload, function(err, msg) {});
 
-    res.status(200).send({
-        message: 'Success!'
-    });
+      if (payload.segment) {
+        Segment.identify({
+          anonymousId: payload.segment.anonId,
+          userId: payload.email,
+          traits: payload.segment.traits
+        });
+
+        Segment.track({
+          anonymousId: payload.segment.anonId,
+          userId: payload.email,
+          event: payload.segment.event,
+          properties: payload.segment.properties
+        });
+      }
+      break;
+  }
+
+  res.status(200).send({
+    message: 'Success!'
+  });
 }
 
 module.exports = handler;
